ea628cefae1f3f35f19dabd841b624f9955302fe
Eugenia Morales <eumorale@calpoly.edu>
2022-07-17 16:35:00 -0700

    basic UI complete for welcome page. Need to set actions for buttons and connect with SQL

--- project/src/main/java/com/example/project/HelloController.java
+++ project/src/main/java/com/example/project/HelloController.java
@@ -11,4 +11,8 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    /*include target inside the xml.. example:
+    <Label fx:id="welcomeText"/> (above the button)
+
+    on mouse press goes the method name */
 }
