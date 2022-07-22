e44f4d93492f0f71003768facac058c31f81d557
PanyaOu <75552913+PanyaOu@users.noreply.github.com>
2022-07-21 22:05:00 -0700

    Query 1 and Query 7 added from Docs

--- project/src/main/java/com/example/project/SuccessfulController.java
+++ project/src/main/java/com/example/project/SuccessfulController.java
@@ -10,12 +10,17 @@ import java.sql.*;
 
 public class SuccessfulController {
     @FXML
+    private MenuButton QueryMenu;
+    @FXML
     private MenuItem testMenu;
     @FXML
     private TextArea queryResults;
     @FXML
     private MenuItem UsernamePassword;
-
+    @FXML
+    private MenuItem QueryFoodByIngredient;
+    @FXML
+    private TextField UserQueryInput;
     @FXML
     private MenuItem Query1;
     @FXML
@@ -35,6 +40,10 @@ public class SuccessfulController {
     @FXML
     private MenuItem mealItem;
 
+    @FXML
+    private MenuItem NumberUsersOnApp;
+
+    Boolean QueryFoodByIngredientTF = false;
     PreparedStatement ps;
     Connection connection;
     public SuccessfulController() throws SQLException {
@@ -46,8 +55,8 @@ public class SuccessfulController {
             e.printStackTrace();
         }
     }
-    private void HandleMouse(MouseEvent event) {
 
+    private void HandleMouse(MouseEvent event) {
         EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
                 String t = ((MenuItem) e.getSource()).getText();
@@ -58,6 +67,8 @@ public class SuccessfulController {
 
     @FXML
     private void testQuery(ActionEvent event){
+        QueryFoodByIngredientTF = false;
+        QueryMenu.setText("Test");
         StringBuilder hi= new StringBuilder();
         int i = 0;
         while (i < 10){
@@ -66,11 +77,18 @@ public class SuccessfulController {
             hi.append(s).append("\n");
         }
         queryResults.setText(String.valueOf(hi));
+    }
 
+    @FXML
+    private void setQuery2True(){
+        QueryFoodByIngredientTF = true;
+        QueryMenu.setText("Search by Ingredient Name. (Insert Ingredient Name)");
     }
 
     @FXML
     private void QueryResult2() throws SQLException {
+        QueryFoodByIngredientTF = false;
+        QueryMenu.setText("What are the usernames and passwords for all users?");
         ResultSet rs;
         Statement statement = connection.createStatement();
         rs = statement.executeQuery("SELECT UserID, password FROM User");
@@ -79,8 +97,46 @@ public class SuccessfulController {
         while (rs.next()){
             String Username = rs.getString(1);
             String Password = rs.getString(2);
-
             ans.append(Username).append(",").append(Password).append("\n");
+        }
+        queryResults.setText(String.valueOf(ans));
+    }
+
+    @FXML
+    private void QueryByInput() throws SQLException{
+        if (QueryFoodByIngredientTF){
+            if (!UserQueryInput.getText().isEmpty()){
+                ResultSet rs;
+                Statement statement = connection.createStatement();
+                rs = statement.executeQuery("select foodName" +
+                        " from Food F" +
+                        " where F.ingredients " +
+                        " LIKE '%" + UserQueryInput.getText() + "%'");
+                StringBuilder ans = new StringBuilder();
+                while (rs.next()){
+                    String FoodName = rs.getString(1);
+                    ans.append(FoodName).append("\n");
+                }
+                queryResults.setText(String.valueOf(ans));
+            }
+            else {
+                queryResults.setText("Enter an ingredient.");
+            }
+        }
+    }
+
+    @FXML
+    private void UsersOnApp() throws SQLException {
+        QueryFoodByIngredientTF = false;
+        QueryMenu.setText("How many users are using this App?");
+        ResultSet rs;
+        Statement statement = connection.createStatement();
+        rs = statement.executeQuery("select count(*) as totUsers from User U");
+        StringBuilder ans = new StringBuilder();
+
+        while (rs.next()){
+            String NumberOfUsers = rs.getString(1);
+            ans.append(NumberOfUsers).append("\n");
         }
         queryResults.setText(String.valueOf(ans));
     }
